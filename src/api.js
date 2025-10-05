57bc8ec4555c6470e7ea52657c9ff6863a94fce2
MohamadxMortuz <122732644+MohamadxMortuz@users.noreply.github.com>
2025-10-05 16:46:11 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+const api = axios.create({ baseURL: 'https://server-helpdesk-mini.onrender.com' });
 
 export const setAuth = (token) => { api.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : undefined; };
 export default api;
